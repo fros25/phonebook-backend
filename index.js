661ae8c3911f67ad0bb348df1d7a5a573ed7cf1a
fros25 <244653891+fros25@users.noreply.github.com>
2026-09-05 11:29:54 +0900

    Complete 3.2: Phonebook backend step 2

--- index.js
+++ index.js
@@ -24,17 +24,24 @@ const persons = [
   },
 ];
 
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/', (request, response) => {
-    response.send("<h1>Hello World!</h1>");
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons);
-})
+app.get("/info", (request, response) => {
+  const number = persons.length;
+  const time = new Date().toString();
+  console.log(time);
+  response.send(
+    `<div>Phonebook has info for ${number} people</div><div>${time}</div>`,
+  );
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server runnig on port ${PORT}`);
-    
-})
+  console.log(`Server runnig on port ${PORT}`);
+});

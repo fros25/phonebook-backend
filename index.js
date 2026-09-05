bee0e0ca55def125f2211db98ba45131e488dbba
fros25 <244653891+fros25@users.noreply.github.com>
2026-09-05 12:55:07 +0900

    Complete 3.4: Phonebook backend step 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -41,13 +41,12 @@ app.get("/info", (request, response) => {
   );
 });
 
-
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   const person = persons.find((person) => person.id === id);
 
   console.log(person);
-  
+
   if (person) {
     response.json(person);
   } else {
@@ -55,8 +54,13 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server runnig on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

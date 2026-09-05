a96dfa8bfc1e4ca41e287ad217ba7fce41a11c42
fros25 <244653891+fros25@users.noreply.github.com>
2026-09-05 14:22:25 +0900

    Complete 3.5 and 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -58,6 +60,45 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
+});
+
+// const generateId = () => {
+//   const maxId =
+//     persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0;
+//   return String(maxId + 1);
+// };
+
+const generateId = () => String(Math.floor(Math.random() * 1000000));
+
+const isDuplicateName = (name) => {
+  const names = persons.map((p) => p.name);
+  return names.includes(name);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "The name or number is missing",
+    });
+  }
+
+  if (isDuplicateName(body.name)) {
+    return response.status(400).json({
+      error: "The name already exists in the phonebook",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  console.log(person);
+  response.json(person);
 });
 
 const PORT = 3001;
